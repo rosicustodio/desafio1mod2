359695ce3ef5e3fdc8aa99b88c516870a6428d2e
rosicustodio <rosimarcustodio@gmail.com>
2021-05-22 17:43:51 -0300

    desafio 1

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Rosimar"
+let idade = 47
+let ano = 2021
+let anoNascimento = (ano-idade)
+
+console.log("meu ano de nascimento é", anoNascimento)
+
+let souhumano = "true"
+console.log("Eu sou humano",souhumano)
+
+let number = 18
+let numero = 10
+
+console.log(number+numero)
